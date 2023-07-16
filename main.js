fe888d7f66b279dc8c0b1fadcb319a760da0e459
JitHoon <imessi0336@gmail.com>
2023-07-17 00:04:13 +0900

    Feat: Create fetchMovie()

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Setting");
+import axios from 'axios';
+
+function fetchMoovie() {
+	axios
+    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+    .then((response) => {
+			console.log(response);
+		})
+};
+
+fetchMoovie();
