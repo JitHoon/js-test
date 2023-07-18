5a1f70e2cefc1466c0457e3c440560293dc919a6
JitHoon <imessi0336@gmail.com>
2023-07-18 16:41:43 +0900

    이지미 로딩 페이지 만들기 (콜백 함수)

---
+++ js/loadImg.js
@@ -0,0 +1,20 @@
+const imgDiv = document.querySelector('.imgDiv');
+
+createImg('https://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg');
+
+function createImg(url) {
+  const img = document.createElement('img');
+  img.src = url;
+  img.addEventListener('load', loadImg(img));
+};
+
+function loadImg(img) {
+  setTimeout(() => {
+    showImg(img);
+  }, 2000);
+}
+
+function showImg(img) {
+  imgDiv.innerHTML = '';
+	imgDiv.append(img);
+};
